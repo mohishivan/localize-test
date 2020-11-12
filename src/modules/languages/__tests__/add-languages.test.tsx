7ecec5548b7bfefdfd6a425236cd64d90ccbb838
Ivan <mohishivan@gmail.com>
2020-11-12 13:15:57 +0200

    Add Test: AddLanguageButton onClick opens Modal

---
+++ src/modules/languages/__tests__/add-languages.test.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../../store";
+import { AddLanguage } from "../add-language";
+
+const wrapper = ({ children }: any) => (
+	<Provider store={store}>{children}</Provider>
+);
+	
+jest.mock("react", () => ({
+	...jest.requireActual("react"),
+	useState: jest.fn()
+}));
+
+test.only("Click on AddLanguage open Modal [test React.useState]", () => {
+
+	const toggleOpen = jest.fn();
+	const toggleOpenMock: any = (initState: boolean) => [initState, toggleOpen];
+
+	jest.spyOn(React, 'useState').mockImplementation(toggleOpenMock);
+	
+	render(<AddLanguage />, { wrapper });
+  fireEvent.click(screen.getByText(/Add language/i))
+  expect(toggleOpen).toHaveBeenCalledTimes(1)
+});
