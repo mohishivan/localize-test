94b9ad09c47591eb47dce47fa96f42d2a155b146
Ivan <mohishivan@gmail.com>
2020-11-13 22:30:55 +0200

    Add theme, and styled-components ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import theme from './theme';
 import { store, persistor } from './store';
 
 ReactDOM.render(
     <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <App />
-        </PersistGate>
+        <ThemeProvider theme={theme}>
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
+        </ThemeProvider>
     </Provider>,
     document.getElementById('root'),
 );
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+const theme = {
+    colors: {
+        primary: '#5489DC',
+        black: '#000',
+        gray: ' #888',
+    },
+    breakpoints: {
+        xs: '0',
+        sm: '600px',
+        md: '960px',
+        lg: '1280px',
+        xl: '1920px',
+    },
+};
+export default theme;
